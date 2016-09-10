24919e75e7649362158bd0b4b09e9eaf5e12342c
Andrew Carr <acarr@mail.utexas.edu>
2016-09-10 10:47:17 -0500

    added a couple of base files

---
+++ assets/scripts/firebase.freeloader.js
@@ -0,0 +1,13 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
+  };
+  
+firebase.initializeApp(config);
+
+var database = firebase.database();
